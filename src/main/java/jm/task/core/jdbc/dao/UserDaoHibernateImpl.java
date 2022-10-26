dcc2c6fdde5594bb9922949091946480c2f37f21
Schurov Konstantin <shurovkp@gmail.com>
2022-10-26 16:11:16 +0300

    first task

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -4,56 +4,77 @@ import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
-
+    private SessionFactory factory = Util.getSessionFactory();
 
     public UserDaoHibernateImpl() {
 
     }
 
-
-
     @Override
     public void createUsersTable() {
-        UserDaoJDBCImpl makeTable = new UserDaoJDBCImpl();
-        makeTable.createUsersTable();
+        String create = "CREATE TABLE IF NOT EXISTS user (id INT not null primary key auto_increment, name VARCHAR(40), " +
+                "lastname VARCHAR(40), age INT)";
+
+        try (Connection connection = Util.getConnection();
+             Statement statement = connection.createStatement()) {
+
+            statement.executeQuery(create);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void dropUsersTable() {
-        UserDaoJDBCImpl dropTable = new UserDaoJDBCImpl();
-        dropTable.dropUsersTable();
+        String drop = "DROP TABLE IF EXISTS user";
+
+        try (Connection connection = Util.getConnection();
+             Statement statement = connection.createStatement()) {
+
+            statement.executeQuery(drop);
+        } catch(SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-        try (SessionFactory factory = Util.getSessionFactory();
-             Session session = factory.getCurrentSession()) {
-
-            session.beginTransaction();
+        Transaction tx = null;
+        try (Session session = factory.openSession()) {
+            tx = session.beginTransaction();
             session.save(new User(name, lastName, age));
             session.getTransaction().commit();
             System.out.println("User с именем – " + name + " добавлен в базу данных");
         } catch (Exception e) {
+            if (tx != null) {
+                tx.rollback();
+            }
             e.printStackTrace();
         }
     }
 
     @Override
     public void removeUserById(long id) {
-        try (SessionFactory factory = Util.getSessionFactory();
-             Session session = factory.getCurrentSession()) {
-
-            session.beginTransaction();
+        Transaction tx = null;
+        try (Session session = factory.openSession()) {
+            tx = session.beginTransaction();
             User user = session.get(User.class, id);
             session.delete(user);
             session.getTransaction().commit();
         } catch (Exception e) {
+            if (tx != null) {
+                tx.rollback();
+            }
             e.printStackTrace();
         }
     }
@@ -61,12 +82,15 @@ public class UserDaoHibernateImpl implements UserDao {
     @Override
     public List<User> getAllUsers() {
         List<User> users = new ArrayList<>();
-        try (SessionFactory factory = Util.getSessionFactory();
-             Session session = factory.getCurrentSession()) {
-
-            session.beginTransaction();
+        Transaction tx = null;
+        try (Session session = factory.openSession()) {
+            tx = session.beginTransaction();
             users = session.createQuery("from User").getResultList();
+            session.getTransaction().commit();
         } catch (Exception e) {
+            if (tx != null) {
+                tx.rollback();
+            }
             e.printStackTrace();
         }
         return users;
@@ -74,7 +98,7 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void cleanUsersTable() {
-        UserDaoJDBCImpl table = new UserDaoJDBCImpl();
-        table.cleanUsersTable();
+        dropUsersTable();
+        createUsersTable();
     }
 }
